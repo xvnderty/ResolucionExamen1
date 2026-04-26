95ee57ce627b47fda0550a51477e986337af463d
frankz <frankz@LAPTOP-G0A5QO8O>
2026-04-25 20:10:15 -0500

    ejerciCio1

--- Examen1/src/examen1/ejercicio1.java
+++ Examen1/src/examen1/ejercicio1.java
@@ -8,44 +8,45 @@ package examen1;
  *
  * @author frankz
  */
+
 import java.util.Scanner;
 public class ejercicio1 {
     public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
         int hora;
         int minuto;
         int segundo;
         
-        Scanner lector = new Scanner(System.in);
-        
-        System.out.println("INGRESE HORAS: ");
+        System.out.println("ingrese hora: ");
         hora = lector.nextInt();
-        System.out.println("INGRESE MINUTOS : ");
+        System.out.println("Ingrese minuto");
         minuto = lector.nextInt();
-        System.out.println("INGRESE SEGUNDOS:");
+        System.out.println("ingrese segubndo ");
         segundo = lector.nextInt();
         
-        boolean condicion1 = (hora>=0 && hora<=24 && minuto>=0&&minuto<=59&&segundo>=0&&segundo<=59);
+        boolean condicion1 = (hora>=0 && hora<=23&&
+                             minuto>= 0 && minuto<=59&&
+                             segundo>=0 && segundo<=59);
         
         if(condicion1){
-            String horaInicial = String.format("ENTRADA: %d : %d : %d",hora,minuto,segundo);
+            String hora1 = String.format("%d : %d : %d",hora,minuto,segundo);
             
             segundo++;
-            if(segundo>=59){
+            
+            if(segundo >= 59){
                 segundo = 0;
                 minuto ++;
             }
-            if (minuto >= 59){
+            if(minuto>=59){
                 minuto = 0;
-                hora++;
+                hora ++;
             }
-            if(hora >=23){
-                hora =0;
+            if(hora >= 23){
+                hora = 0;
             }
-            
-            System.out.printf("%s -> Salida : %d:%d:%d",horaInicial,hora,minuto,segundo);
+            System.out.printf("Entrada->%s , SALIDA-> %d : %d : %d ",hora1,hora,minuto,segundo);
         }else{
             System.out.println("INGRESE UN DATO VALIDO");
         }
-        
     }
 }
